8f13f896969d289d92a9372f7a8cde3db3457ae8
Ali Naqi <alinaqi.te@gmail.com>
2025-07-26 22:24:02 +0000

    chota size and speed super fast

--- src/components/ui/cursor-follower.tsx
+++ src/components/ui/cursor-follower.tsx
@@ -20,14 +20,14 @@ export function CursorFollower() {
 
   return (
     <div
-      className="pointer-events-none fixed z-[101] hidden h-24 w-24 -translate-x-1/2 -translate-y-1/2 lg:block"
+      className="pointer-events-none fixed z-[101] hidden h-12 w-12 -translate-x-1/2 -translate-y-1/2 lg:block"
       style={{
         left: `${position.x}px`,
         top: `${position.y}px`,
-        transition: 'left 0.1s ease-out, top 0.1s ease-out',
+        transition: 'left 0.05s ease-out, top 0.05s ease-out',
       }}
     >
-        <div className="w-full h-full animate-[spin_8s_linear_infinite] rounded-full border-2 border-dashed border-primary">
+        <div className="w-full h-full animate-[spin_1s_linear_infinite] rounded-full border-2 border-dashed border-primary">
         </div>
     </div>
   );
